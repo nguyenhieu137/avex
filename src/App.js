c55b49f67944e4fe7f1e35b910502b438722f646
nguyenhieu137 <nguyenhieu1372k@gmail.com>
2024-02-19 15:38:28 +0700

    Add app.css.map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home"
+import About from "./components/About/About";
+import Project from "./components/Project/Project";
+import ProjectDetail from './components/Project-details/ProjectDetail';
+import Contact from "./components/Contact/Contact";
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header></Header>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/product" element={<Home></Home>}></Route>
+        <Route path="/project" element={<Project></Project>}></Route>
+        <Route path=":name" element={<ProjectDetail></ProjectDetail>}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
+        <Route path="/new-products" element={<Home></Home>}></Route>
+        <Route path="/featured-products" element={<Home></Home>}></Route>
+        <Route path="/hot-products" element={<Home></Home>}></Route>
+      </Routes>
+      <Footer></Footer>
+    </>
   );
 }
 
